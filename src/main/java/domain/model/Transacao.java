8b188dd98ae6715dac4ba35128bc4ad294248036
Eduardo Marionucci <dududmdias@gmail.com>
2025-11-05 13:18:57 -0300

    feat: transação e entidade

---
+++ src/main/java/domain/model/Transacao.java
@@ -0,0 +1,15 @@
+package domain.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Transacao {
+
+    private String e2e;
+    private BigDecimal amount;
+    private int payerId;
+    private int receiverId;
+    private Date liquidation;
+
+
+}
